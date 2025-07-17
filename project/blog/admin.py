388c744ee078bf325b9b09601142bb9214a278ba
baxt <habibulaevhushbaht05@gmail.com>
2025-07-17 16:04:23 +0600

    added category

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import News,NewsImage,Category
 # Register your models here.
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(NewsImage)
+class NewsImageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(
+        verbose_name="Заголовок",
+        max_length=100,
+    )
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+    
+
+
+class News(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="Кат",
+        verbose_name="Кат",
+        blank=True,
+        null=True
+    )
+    title = models.CharField(
+        verbose_name="Заголовок",
+        max_length=100,
+    )
+    description = models.TextField(
+        verbose_name="Оописание",
+        blank=True,
+        null=True,
+        default="По умол.описание"
+    )
+    
+    class Meta:
+        verbose_name = "Новости"
+        verbose_name_plural = "Новости"
+    
+    def __str__(self):
+        return self.title
+
+
+class NewsImage(models.Model):
+    news = models.ForeignKey(
+        News,
+        on_delete=models.CASCADE,
+        related_name="images",
+        verbose_name="Новости"
+    )
+    image = models.ImageField(
+        "Фото",
+        upload_to="news/photos",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Фото новостей'
+        verbose_name_plural = 'Фото новостей'
+    
+    def __str__(self):
+        return f"Фото-{self.news.title}"
+
