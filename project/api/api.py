9741433f651a48322424669af50c38a456c19b7b
baxt <habibulaevhushbaht05@gmail.com>
2025-09-10 11:08:33 +0600

    aded api

--- project/api/api.py
+++ project/api/api.py
@@ -1,7 +1,52 @@
-from rest_framework.viewsets import ModelViewSet
-from .serializers import News,NewsSerializer
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions,status
 
 
-class NewsModelViewset(ModelViewSet):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
+from blog.models import News,Category
+from .serializers import NewsSerializer
+
+class NewsListAPIView(APIView):
+    def get(self, request, format=None):
+        news = NewsSerializer(News.objects.all(),many=True).data
+        return Response(news,status=status.HTTP_200_OK)
+
+
+    def post(self, request, format=None):
+        news_data=request.data
+        serializer_data = NewsSerializer(data=news_data)
+        
+        if serializer_data.is_valid():
+            data = serializer_data.save()
+            return Response(serializer_data.data,status=status.HTTP_201_CREATED)
+
+        return Response(serializer_data.errors)
+
+
+class NewsDetailAPIView(APIView):
+
+    def get_object(self,pk):
+        return get_object_or_404(News,pk=pk)
+
+    def get(self,request,pk,format=None,*args,**kwargs):
+        news = self.get_object(pk)
+        news_data = NewsSerializer(news).data
+        return Response(news_data,status=status.HTTP_200_OK)
+
+
+    def delete(self,request,pk,format=None,*args,**kwargs):
+        news = self.get_object(pk)
+        print(news)
+        return Response({"detail":f"Object news with id {news.id} was deleted"},status=status.HTTP_204_NO_CONTENT)
+
+    def put(self,request,pk,format=None,*args,**kwargs):
+        news = self.get_object(pk)
+        news_data = NewsSerializer(news,data=request.data)
+        if news_data.is_valid():
+            news_data.save()
+            return Response(news_data.data) 
+        
+        return Response(news_data.errors)
--- project/api/enpoints.py
+++ project/api/enpoints.py
@@ -2,13 +2,15 @@ from django.urls import path,include
 
 from rest_framework.routers import DefaultRouter
 
-from .api import NewsModelViewset
+from .api import NewsListAPIView,NewsDetailAPIView
 
 router = DefaultRouter()
 
-router.register('news',NewsModelViewset)
+# router.register('news',NewsModelViewset)
 
 
 urlpatterns = [
+    path('news/', NewsListAPIView.as_view(), name='news-list'),
+    path('news/<int:pk>/', NewsDetailAPIView.as_view(), name='news-delete'),
     path('',include(router.urls))
 ]
